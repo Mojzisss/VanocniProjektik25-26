e157858f61300459577aa0d810f52677d6b26a11
tomanek <moojmirmojma@gmail.com>
2026-01-16 11:04:59 +0100

    piece of input things added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
+        ObjectMapper parser = new ObjectMapper();
+
+        try{
+            InputStream input = new FileInputStream("resource/characters");
+            Character character =parser.readWalue(input , Character.class);
+            System.out.println(character);
+
+
+        }
    }
 }
