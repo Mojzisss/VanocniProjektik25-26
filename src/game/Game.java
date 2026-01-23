859abda5267ab4236e89e2c0d7c81120b6f9a9a3
tomanek <moojmirmojma@gmail.com>
2026-01-23 10:59:05 +0100

    game class improve

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,4 +2,20 @@ package game;
 import java.util.HashMap;
 import commands.*;
 public class Game {
+
+    private GameData world;
+    private Player player;
+    private HashMap<String, Command> commands;
+    public void inicialization(){
+        commands = new HashMap<>();
+        world = GameData.loadGameDataFromResources("/gamedata.json");
+        //TODO pridat commands
+        commands.put("pohyb", new Movement(player));
+
+    }
+
+    public void start(){
+        inicialization();
+        //zde bude herni smycka
+    }
 }
